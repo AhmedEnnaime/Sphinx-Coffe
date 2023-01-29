d656894f226e29dd5f7d20950bae4d4c68012607
AhmedEnnaime <97989224+AhmedEnnaime@users.noreply.github.com>
2023-01-29 14:45:31 +0100

    Initial Commit

---
+++ Sphinx Coffee/Sphinx_CoffeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sphinx_CoffeeApp.swift
+//  Sphinx Coffee
+//
+//  Created by Ahmed Ennaime on 29/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct Sphinx_CoffeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
